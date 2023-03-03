f8cd1ae93d4f3d7f4a09dab3b029caf075c8491c
aharo-lumificyber <aharo@lumificyber.com>
2023-03-02 21:01:28 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+import TodoList from './modules/TodoList';
+
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList />
+     <h1>Have a Great Day</h1>
+    </div>
+  );
+}
+
+export default App;
